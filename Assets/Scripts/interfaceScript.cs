89aebf924074f8c7b191550f4a526d0981797fdb
Jacob Le <jle9629@sdsu.edu>
2021-05-07 00:04:16 -0700

    made some updates to the interface menu, still brainstorming

---
+++ Assets/Scripts/interfaceScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class interfaceScript : MonoBehaviour
+{
+
+    public TMP_Dropdown sub1, sub2, sub3, sub4;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        // sub1 = GameObject.Find("")
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+}
